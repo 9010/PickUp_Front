331163d61e151e92ca56418f5feed69720cab06d
zx <1106690435@qq.com>
2019-11-05 14:29:52 +0800

    init

---
+++ js/CommonUtil.js
@@ -0,0 +1,16 @@
+var CommonUtils = {
+    urlPrefix :'http://47.98.107.94:8080/stu/',  //prefix of api address
+    urlPrefixTeacher :'http://47.98.107.94:8080/stu/teacher/',
+
+    //urlPrefix :'http://stu.ngrok.ilingyue.cn:8088/stu/',  //prefix of api address
+    //urlPrefixTeacher :'http://stu.ngrok.ilingyue.cn:8088/stu/teacher/',
+
+    //urlPrefix :'http://192.168.2.1:8080/',  //prefix of api address
+    //urlPrefixTeacher :'http://192.168.2.1:8080/teacher/',
+
+    //urlPrefix :'http://stu.free.cngrok.com/stu/',  //prefix of api address
+    //urlPrefixTeacher :'http://stu.free.cngrok.com/stu/teacher/',
+
+    //urlPrefix :'http://10.254.2.62/',  //prefix of api address
+    //urlPrefixTeacher :'http://10.254.2.62/teacher/',
+}
